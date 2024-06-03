b908746fe957a9ee22b94bac5b288559412be2b1
laurens-in <laurensinauen@gsnail.ch>
2024-06-04 00:04:46 +0200

    fix func codes

--- src/arcane/arcane.h
+++ src/arcane/arcane.h
@@ -1,10 +1,10 @@
 // ARCANE function codes
-#define FUNC_NMT     0b00000000000
-#define FUNC_MIDI0   0b00010000000
-#define FUNC_MIDI1   0b00100000000
-#define FUNC_MIDI2   0b00110000000
-#define FUNC_CFGW    0b01000000000
-#define FUNC_CFGR    0b01010000000
+#define FUNC_NMT     0b0000
+#define FUNC_MIDI0   0b0001
+#define FUNC_MIDI1   0b0010
+#define FUNC_MIDI2   0b0011
+#define FUNC_CFGW    0b0100
+#define FUNC_CFGR    0b0101
 
 #define NMT_OFF      0b0000      
 #define NMT_ON       0b0001
